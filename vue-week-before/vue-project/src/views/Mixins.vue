846ebfeef6f02b2e3882648f985c56e563be1ce7
vencott <vencottkim@gmail.com>
2022-02-21 15:13:38 +0900

    09 Reusability & Composition

---
+++ vue-week-before/vue-project/src/views/Mixins.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>{{ htmlString }}</div>
+    <div v-html="htmlString"></div>
+  </div>
+</template>
+
+<script>
+import ApiMixin from "../api.js";
+
+export default {
+  mixins: [ApiMixin],
+  data() {
+    return {
+      productList: [],
+    };
+  },
+  async mounted() {
+    this.productList = await this.$callAPI(
+      "https://3d5348c0-4158-4724-ad6f-352e7ef79ceb.mock.pstmn.io/list",
+      "get"
+    );
+    console.log(this.productList);
+  },
+};
+</script>
