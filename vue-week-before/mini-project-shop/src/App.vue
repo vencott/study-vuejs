0953a62652b3146eb85ba8df3d4222f88e731978
vencott <vencottkim@gmail.com>
2022-02-23 01:19:57 +0900

    제품 판매 웹앱 구현

--- vue-week-before/mini-project-shop/src/App.vue
+++ vue-week-before/mini-project-shop/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { Header, Footer },
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
   },
 };
 </script>
@@ -21,6 +26,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
